c1cc19c878055d22fbb8e90605e7cef2b9c58c70
sumitrana1505 <68266813+sumitrana1505@users.noreply.github.com>
2022-04-05 21:38:47 +0530

    drumkit

---
+++ index.js
@@ -0,0 +1,56 @@
+
+for (var i = 0; i < 7; i++){
+
+    document.querySelectorAll(".drum")[i].addEventListener("click" , handleClick)
+    function handleClick() {
+
+        var buttonInnerHtml = this.innerHTML;
+        makesound(buttonInnerHtml);
+    };
+}
+
+    document.addEventListener("keypress",function(event){
+        makesound(event.key)
+    });
+
+    function makesound(key){
+
+        switch (key) {
+            case "w":
+                var tom1 = new Audio('sounds/tom-1.mp3');
+                tom1.play();
+                break;
+
+            case "a":
+                var tom2 = new Audio('sounds/tom-2.mp3');
+                tom2.play();
+                break;
+            case "s":
+                var tom3 = new Audio('sounds/tom-3.mp3');
+                tom3.play();
+                break;
+            case "d":
+                var tom4 = new Audio('sounds/tom-4.mp3');
+                tom4.play();
+                break;
+            case "j":
+                var audio = new Audio('sounds/snare.mp3');
+                audio.play();
+                break;
+            case "k":
+                var audio = new Audio('sounds/crash.mp3');
+                audio.play();
+                break;
+            case "l":
+                var audio = new Audio('sounds/kick-bass.mp3');
+                audio.play();
+                break;
+          }
+    }
+
+
+
+
+
+// var audio = new Audio('sounds/tom-1.mp3');
+// audio.play();
